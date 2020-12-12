36650ca26de678c61f2c6ead746c9f87342fa56b
nym <nym@nym.local>
2020-12-11 23:29:24 -0600

    Updated Template

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,8 @@
 import React from 'react';
+import Component from './components/Component';
 
-const Hello = ({ name }) => {
+export default function App() {
   return (
-    <h1>
-      Hello,
-      {name}
-    </h1>
+    <Component />
   );
-};
-
-export default Hello;
+}
